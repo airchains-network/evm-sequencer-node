c31e29a6006ea10ceabecaf989baf3d678b9cb06
computerkeeda <samwodson@gmail.com>
2023-12-24 15:18:34 +0530

    Update RPC URLs in constants.go

--- common/constants.go
+++ common/constants.go
@@ -5,8 +5,8 @@ import "os"
 
 const BatchSize = 25
 const BlockDelay = 5
-const ExecutionClientRPC = "http://35.244.25.153/jsonrpc"
-const SettlementClientRPC = "http://192.168.1.105:8080"
+const ExecutionClientRPC = "http://localhost/8545/"
+const SettlementClientRPC = "http://localhost:8080"
 const KeyringDirectory = "./account/keys"
 var DaClientRPC = os.Getenv("DA_CLIENT_RPC")
 // const DaClientRPC = "http://localhost:5050/celestia"
